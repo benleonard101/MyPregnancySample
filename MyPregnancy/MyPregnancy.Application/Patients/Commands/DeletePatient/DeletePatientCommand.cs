1dcde3b74e31c19322901daaeabce7b6fc0c9031
Ben Leonard <bleonard@cayan.com>
2020-02-04 00:48:18 +0000

    Removed update and delete - to be finished with unit tests if time

--- MyPregnancy/MyPregnancy.Application/Patients/Commands/DeletePatient/DeletePatientCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MyPregnancy.Application.Patients.Commands.DeletePatient
-{
-    using MediatR;
-
-    public class DeletePatientCommand : IRequest
-    {
-        public int PatientId { get; set; }
-    }
-}
